80e20d91c8878e838290b7970687420f5ee62b39
Oliver Sverko <osverko99@gmail.com>
2025-09-17 00:03:58 -0400

    Add files via upload

---
+++ Lobby/ReplicatedStorage/ClassData.lua
@@ -0,0 +1,37 @@
+
+
+local ClassData = {
+
+	-- ONLY USED FOR FALLBACK
+	default_class = {
+		hp = 100,
+		stamina = 100,
+		damage = 1.0,
+		speed = 1.0,
+		startingWeapon = "Bretta",
+		startingPerk = nil
+	},
+
+	Rogue = {
+		hp = 100,
+		stamina = 100,
+		damage = 1.1,
+		speed = 1.0,
+		startingWeapon = "P90",
+		startingPerk = nil
+	},
+
+	class2 = {
+		hp = 100,
+		stamina = 100,
+		damage = 1.0,
+		speed = 1.0,
+		startingWeapon = "Bretta",
+		startingPerk = nil
+	}
+
+
+}
+
+
+return ClassData
